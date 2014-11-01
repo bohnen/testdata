6c8319ac232d05ee2154c307c999234a6dc936ed
bohnen <bohnen.net@gmail.com>
2014-11-02 08:44:19 +0900

    ::insert

--- src/csvimport.rb
+++ src/csvimport.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
+#
 # CSVでテストデータを作成して、DBに入れるスクリプト
+# gem install sequel が必要。JDBCを利用する場合は、jrubyで実行する必要がある
+#
 
 
 # CSVImport名前空間
@@ -8,12 +11,15 @@ module CSVImport
 	require 'sequel'
 
 	# CSVを読み込んで、一連のHashを作成する
+	#
 	# CSVは下記のような構成になっている
 	# cfg.param1, cfg.param2, cfg.param3, a.column1, a.column2, b.column1, b.column3, ...
 	# ここでcfgは固定の制御パラメータ（データとしてはDBに保存されない）、a, bはインスタンスの識別子である。
 	# 同じテーブルに同時に複数行Insertしなければいけないケースもあるため、インスタンスを識別できるようにする。
 	# 結果として、{:cfg => {:param1 => val1, :param2 => val2}, :a => {:column1 => val1, column2 => val2}...}
 	# という、入れ子のHashを作成する
+	#
+	# @param file CSVファイルのパス 
 	def parse(file)
 		records = [] 
 		csv = CSV.read(file, headers:true)
@@ -32,5 +38,27 @@ module CSVImport
 		records
 	end
 
-	module_function :parse
+	# テーブルにデータをinsertする
+	#
+	# records のそれぞれの行のHashに入っている順にinsertを行う
+	# トランザクショナルにしたければこのメソッドを DB.transaction do - end で囲うこと。
+	#
+	# @param db [Sequel::Database] 接続するDB。初期化済みのこと
+	# @param records [Array] insertするデータ。parseで作成した形式
+	# @param table_map [Hash] objとtable名の対応。
+	# @param dry_run [Boolean] 実行せずinsert文を返す
+	def insert(db, records=[], table_map={}, dry_run=false)
+		records.map do |r|
+			r.map do |key, val|
+				table_name = table_map[key].to_sym || key.to_sym
+				if dry_run then
+					db[table_name].insert_sql(val)
+				else
+					db[table_name].insert(val)
+				end
+			end
+		end
+	end
+
+	module_function :parse, :insert
 end
--- test/csvinsert_spec.rb
+++ test/csvinsert_spec.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require '../src/csvimport.rb'
+require 'logger'
 
 RSpec.describe CSVImport do
 	describe '#parse' do
@@ -11,6 +12,29 @@ RSpec.describe CSVImport do
 			expect(@csv[0].size).to be > 0
 			expect(@csv[0].keys).to contain_exactly("cfg","foo","bar")
 			expect(@csv[1]["cfg"]).to be_an_instance_of(Hash)
+		end
+	end
+
+	describe '#insert' do
+		before(:all) do
+			@db = Sequel.connect('mock://db2')
+			@db.loggers << Logger.new($stdout)
+			@table_map = {"cfg" => "DUAL", "foo" => "T_FOO", "bar" => "T_BAR"}
+			@csv = CSVImport.parse("./data/sample.csv")
+		end  
+
+		it "dry_run returns insert statement" do 
+			rets = CSVImport.insert(@db, @csv, @table_map, true)	
+			rets.each do |r|
+				r.each do |r2|
+					expect(r2).to match(/^INSERT/)
+				end
+			end
+		end
+
+		it "execute insert returns nothing" do
+			rets = CSVImport.insert(@db, @csv, @table_map)
+			puts rets.inspect
 		end
 	end
 end
